a0d9a3f9df50a96d9c3fd45203e0b060ed92dfb5
JamesWicks01 <115103781+JamesWicks01@users.noreply.github.com>
2022-12-12 00:18:51 +0000

    added timers to hunger,tiredness and happiness

--- scripts/decreasePetStats.js
+++ scripts/decreasePetStats.js
@@ -1,29 +1,43 @@
-function decreaseHungerValue() {
-    let petHunger = document.querySelector('#hunger').value--;
-    console.log(petHunger);
+let petHunger = 100;
+let petTiredness = 100;
+let petHappiness = 100;
 
-    if(petHunger > 0) {
+function decreaseHungerValue() {
+    const out = document.querySelector('#hungerValue');
+    out.textContent = petHunger--;
+    if (petHunger > 0) {
         setTimeout(decreaseHungerValue, 1000);
     } else if (petHunger === 0) {
-        petHunger = 0;
+        out.textContent = '0';
     } else {
-        console.error('Error with decreasingHungerValue');
+        console.error("Error with decreaseHungerValue");
     }
-    
 }
 
 function decreaseTirednessValue() {
-    let petTiredness = document.querySelector('#tiredness').value--;
-    console.log(petTiredness);
-
-    if(petTiredness > 0) {
-        setTimeout(decreaseTirednessValue, 10000);
+    const out = document.querySelector('#tirednessValue');
+    out.textContent = petTiredness--;
+    if (petTiredness > 0) {
+        setTimeout(decreaseTirednessValue, 1000);
     } else if (petTiredness === 0) {
-        petTiredness = 0;
+        out.textContent = '0';
     } else {
-        console.error('Error with decreasingTirednessValue');
+        console.error("Error with decreaseTirednessValue");
+    }
+}
+
+function decreaseHappinessValue() {
+    const out = document.querySelector('#happinessValue');
+    out.textContent = petHappiness--;
+    if (petHappiness > 0) {
+        setTimeout(decreaseHappinessValue, 1000);
+    } else if (petHappiness === 0) {
+        out.textContent = '0';
+    } else {
+        console.error("Error with decreaseHappinessValue");
     }
 }
 
 window.addEventListener('load', decreaseHungerValue);
 window.addEventListener('load', decreaseTirednessValue);
+window.addEventListener('load', decreaseHappinessValue);

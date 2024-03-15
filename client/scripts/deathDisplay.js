04fa452dab02d2feb8794d71b8dc21f3ee3116e1
JamesWicks01 <115103781+JamesWicks01@users.noreply.github.com>
2024-03-15 00:16:20 +0000

    Fixed Issues with null data in local storage

--- client/scripts/deathDisplay.js
+++ client/scripts/deathDisplay.js
@@ -1,24 +1,18 @@
-// Function to clear local storage and to change the state of page
 function reset() {
   localStorage.clear();
-  window.location = 'petscreen.html';
+  window.location.replace('/');
   console.log('RESET SUCCESSFUL');
 }
 
-// Function to make the button functionable on the page.
 function button() {
   const ResetButton = document.getElementById('reset');
   ResetButton.addEventListener('click', reset);
 }
 
-// Fucntion that holds all the function that need to be executed on page loaded.
-// Including the loading of values from local storage and displaying them on the screen.
 function init() {
   const pet = JSON.parse(localStorage.getItem('name'));
   const petName = document.getElementById('name');
   const petAliveTime = document.getElementById('timeAlive');
-  const petCauseOfDeath = document.getElementById('CauseOfDeath');
-  petCauseOfDeath.textContent = 'Cause of death: ' + pet.deathtype;
   petName.textContent = 'Your Pet ' + pet.name + ' is died.';
   petAliveTime.textContent = 'It was alive for ' + pet.aliveTime + ' minutes';
 }
